88f0566d8207c9458eeb02559507d59d6b318d9f
hamza-essalhi <hamzaessalhix@gmail.com>
2023-06-08 12:58:21 -0700

    tasks from 0 t0 5

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     """Print 1+2"""
     from add_0 import add
 
-    n1 = 1
-    n2 = 2
-    print("{} + {} = {}".format(n1, n2, add(n1, n2)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
