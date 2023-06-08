7a372e14522ef28c3d1debfc79993666e9c1edc3
hamza-essalhi <hamzaessalhix@gmail.com>
2023-06-08 12:57:05 -0700

    tasks from 0 t0 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     elif counter == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(counter))
     for i in range(counter):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
