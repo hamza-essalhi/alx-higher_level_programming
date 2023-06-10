2022fd5e89854067c91f80ab9f66b518854adbcc
hamza-essalhi <hamzaessalhix@gmail.com>
2023-06-10 13:10:30 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	"""Print list function whit new line"""
-	for i in my_list:
-		print("{}".format(i))
+    for i in my_list:
+        print("{:d}".format(i))
