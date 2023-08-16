eb975cf29a8d12f1fb3d31309491daa673e110df
hamza-essalhi <hamzaessalhix@gmail.com>
2023-08-16 11:36:40 -0700

    all done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,12 @@
--- creates the MySQL server user user_0d_1 with all privileges
--- password should be set to user_0d_1_pwd and 
--- should not fail if user already exists
+-- Copyright (c) 2023 Hamza Essalhi. All rights reserved.
+-- This script creates the MySQL server user user_0d_1
 
+-- Create user user_0d_1 if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
