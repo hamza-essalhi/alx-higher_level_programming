058570b6df2f694fe30b6b507a15ffffb9b01e6e
hamza-essalhi <hamzaessalhix@gmail.com>
2023-08-16 11:24:07 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
--- Copyright (c) 2023 Hamza Essalhi. All rights reserved.
--- This script grants privileges to the MySQL users user_0d_1 and user_0d_2 in localhost
-
+-- script that lists all privileges of the MySQL
+-- users user_0d_1 and user_0d_2 on local server
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
